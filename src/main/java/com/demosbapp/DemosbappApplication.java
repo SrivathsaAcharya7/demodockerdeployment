d3a30ac850869fe4ac6c1a0128eb68785247d030
Srivathsa Acharya R <srivathsaacharya167@gmail.com>
2024-03-21 15:20:54 +0530

    initial commit

---
+++ src/main/java/com/demosbapp/DemosbappApplication.java
@@ -0,0 +1,14 @@
+package com.demosbapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosbappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemosbappApplication.class, args);
+		System.out.println("App Started");
+	}
+
+}
---
+++ src/test/java/com/demosbapp/DemosbappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.demosbapp;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemosbappApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertEquals(true,true);
+	}
+
+}
